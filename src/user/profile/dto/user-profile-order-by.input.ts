f4a72d352971139f446326d72882d0ff06719c7d
Seven Du <shiweidu@outlook.com>
2021-03-31 13:44:05 +0800

    docs: update user profile order by docs

--- src/user/profile/dto/user-profile-order-by.input.ts
+++ src/user/profile/dto/user-profile-order-by.input.ts
@@ -1,41 +1,63 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
 
+/**
+ * User profile order by input
+ */
 @InputType({ description: 'User profile order by input' })
 export class UserProfileOrderByInput implements Prisma.UserProfileOrderByInput {
+
+  /**
+   * `userId` field sort order
+   */
   @Field(() => Prisma.SortOrder, {
     nullable: true,
-    description: 'Field sort order',
+    description: '`userId` field sort order',
   })
   userId?: Prisma.SortOrder;
 
+  /**
+   * `name` field sort order
+   */
   @Field(() => Prisma.SortOrder, {
     nullable: true,
-    description: 'Field sort order',
+    description: '`name` field sort order',
   })
   name?: Prisma.SortOrder;
 
+  /**
+   * `avatar` field sort order
+   */
   @Field(() => Prisma.SortOrder, {
     nullable: true,
-    description: 'Field sort order',
+    description: '`avatar` field sort order',
   })
   avatar?: Prisma.SortOrder;
 
+  /**
+   * `bio` field sort order
+   */
   @Field(() => Prisma.SortOrder, {
     nullable: true,
-    description: 'Field sort order',
+    description: '`bio` field sort order',
   })
   bio?: Prisma.SortOrder;
 
+  /**
+   * `location` field sort order
+   */
   @Field(() => Prisma.SortOrder, {
     nullable: true,
-    description: 'Field sort order',
+    description: '`location` field sort order',
   })
   location?: Prisma.SortOrder;
 
+  /**
+   * `updatedAt` field sort order
+   */
   @Field(() => Prisma.SortOrder, {
     nullable: true,
-    description: 'Field sort order',
+    description: '`updatedAt` field sort order',
   })
   updatedAt?: Prisma.SortOrder;
 }
