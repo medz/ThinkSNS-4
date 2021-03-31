f47a7fa3fa02a7b7b31323dee89e275b51b15565
Seven Du <shiweidu@outlook.com>
2021-03-31 13:50:10 +0800

    docs: update user where docs

--- src/user/dto/user-where.input.ts
+++ src/user/dto/user-where.input.ts
@@ -4,50 +4,83 @@ import { DateTimeFilter, StringFilter } from 'src/graphql';
 import { MomentListRelationFilter } from 'src/moment/dto/moment-list-relation.filter';
 import { UserProfileRelationFilter } from '../profile/dto/user-profile-relation.filter';
 
+/**
+ * User where input
+ */
 @InputType({
   description: 'User where input',
 })
 export class UserWhereInput
   implements Omit<Prisma.UserWhereInput, 'password' | 'authorizationTokens'> {
+  /**
+   * AND where
+   */
   @Field(() => [UserWhereInput], { nullable: true, description: 'AND where' })
   AND?: UserWhereInput[];
 
+  /**
+   * OR where
+   */
   @Field(() => [UserWhereInput], { nullable: true, description: 'OR where' })
   OR?: UserWhereInput[];
 
+  /**
+   * NOT where
+   */
   @Field(() => [UserWhereInput], { nullable: true, description: 'NOT where' })
   NOT?: UserWhereInput[];
 
+  /**
+   * ID filter
+   */
   @Field(() => StringFilter, { nullable: true, description: 'ID filter' })
   id?: StringFilter;
 
+  /**
+   * login name filter
+   */
   @Field(() => StringFilter, {
     nullable: true,
     description: 'login name filter',
   })
   login?: StringFilter;
 
+  /**
+   * E-Mail filter
+   */
   @Field(() => StringFilter, { nullable: true, description: 'E-Mail filter' })
   email?: StringFilter;
 
+  /**
+   * user phone number filter
+   */
   @Field(() => StringFilter, {
     nullable: true,
-    description: 'user phone numberfilter',
+    description: 'user phone number filter',
   })
   phone?: StringFilter;
 
+  /**
+   * created at date time filter
+   */
   @Field(() => DateTimeFilter, {
     nullable: true,
     description: 'created at date time filter',
   })
   createdAt?: DateTimeFilter;
 
+  /**
+   * user profile relation filter
+   */
   @Field(() => UserProfileRelationFilter, {
     nullable: true,
     description: 'user profile relation filter',
   })
   profile?: UserProfileRelationFilter;
 
+  /**
+   * user moment list relation filter
+   */
   @Field(() => UserProfileRelationFilter, {
     nullable: true,
     description: 'user moment list relation filter',
