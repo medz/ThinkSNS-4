b9ec6424a9fc095e90368a71fa1dc71dfeabf507
Seven Du <shiweidu@outlook.com>
2021-03-31 13:48:10 +0800

    docs: update user profile where docs and format

--- src/user/profile/dto/user-profile-where.input.ts
+++ src/user/profile/dto/user-profile-where.input.ts
@@ -3,45 +3,84 @@ import { Prisma } from '@prisma/client';
 import { StringFilter } from 'src/graphql';
 import { UserRelationFilter } from 'src/user/dto/user-relation.filter';
 
+/**
+ * User profile where input
+ */
 @InputType({ description: 'User profile where input' })
 export class UserProfileWhereInput
   implements Omit<Prisma.UserProfileWhereInput, 'updatedAt'> {
+  /**
+   * AND where
+   */
   @Field(() => [UserProfileWhereInput], {
     nullable: true,
     description: 'AND where',
   })
   AND?: UserProfileWhereInput[];
 
+  /**
+   * OR where
+   */
   @Field(() => [UserProfileWhereInput], {
     nullable: true,
     description: 'OR where',
   })
   OR?: UserProfileWhereInput[];
 
+  /**
+   * NOT where
+   */
   @Field(() => [UserProfileWhereInput], {
     nullable: true,
     description: 'NOT where',
   })
   NOT?: UserProfileWhereInput[];
 
+  /**
+   * User ID filter
+   */
   @Field(() => StringFilter, { nullable: true, description: 'User ID filter' })
   userId?: StringFilter;
 
+  /**
+   * User relation filter
+   */
   @Field(() => UserRelationFilter, {
     nullable: true,
     description: 'User relation filter',
   })
   user?: UserRelationFilter;
 
-  @Field(() => StringFilter, { nullable: true, description: 'User ID filter' })
+  /**
+   * User name filter
+   */
+  @Field(() => StringFilter, {
+    nullable: true,
+    description: 'User name filter',
+  })
   name?: StringFilter;
 
-  @Field(() => StringFilter, { nullable: true, description: 'User ID filter' })
+  /**
+   * User avatar filter
+   */
+  @Field(() => StringFilter, {
+    nullable: true,
+    description: 'User avatar filter',
+  })
   avatar?: StringFilter;
 
-  @Field(() => StringFilter, { nullable: true, description: 'User ID filter' })
+  /**
+   * User bio filter
+   */
+  @Field(() => StringFilter, { nullable: true, description: 'User bio filter' })
   bio?: StringFilter;
 
-  @Field(() => StringFilter, { nullable: true, description: 'User ID filter' })
+  /**
+   * User location filter
+   */
+  @Field(() => StringFilter, {
+    nullable: true,
+    description: 'User location filter',
+  })
   location?: StringFilter;
 }
