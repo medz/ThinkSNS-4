9ac548490fc9f9fe8a40d9a47b0e179b1f041b2a
Seven Du <shiweidu@outlook.com>
2021-03-31 13:45:37 +0800

    docs: update user profile relation filter docs

--- src/user/profile/dto/user-profile-relation.filter.ts
+++ src/user/profile/dto/user-profile-relation.filter.ts
@@ -2,15 +2,24 @@ import { Field, InputType } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
 import { UserProfileWhereInput } from './user-profile-where.input';
 
+/**
+ * User profile where filter
+ */
 @InputType({ description: 'User profile where filter' })
 export class UserProfileRelationFilter
   implements Prisma.UserProfileRelationFilter {
+  /**
+   * is where input
+   */
   @Field(() => UserProfileWhereInput, {
     nullable: true,
     description: 'is where input',
   })
   is?: UserProfileWhereInput;
 
+  /**
+   * is not where input
+   */
   @Field(() => UserProfileWhereInput, {
     nullable: true,
     description: 'is not where input',
