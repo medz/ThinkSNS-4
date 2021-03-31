a2df111fcd171359b7402781e66d35a361e6a829
Seven Du <shiweidu@outlook.com>
2021-03-31 13:51:00 +0800

    docs: update user relation docs

--- src/user/dto/user-relation.filter.ts
+++ src/user/dto/user-relation.filter.ts
@@ -2,16 +2,25 @@ import { Field, InputType } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
 import { UserWhereInput } from './user-where.input';
 
+/**
+ * User Relation filter
+ */
 @InputType({
   description: 'User Relation filter',
 })
 export class UserRelationFilter implements Prisma.UserRelationFilter {
+  /**
+   * User relation filter is
+   */
   @Field(() => UserWhereInput, {
     nullable: true,
     description: 'User relation filter is',
   })
   is?: UserWhereInput;
 
+  /**
+   * User relation filter is not
+   */
   @Field(() => UserWhereInput, {
     nullable: true,
     description: 'User relation filter is not',
